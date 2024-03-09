8a7407e35f6dd06842e41ccc0c6d1760514b2696
Valerii <vd.alternative@gmail.com>
2024-03-09 18:23:27 +0200

    Update contacts.js

--- contacts.js
+++ contacts.js
@@ -2,7 +2,7 @@ import fs from "fs/promises";
 import path from "path";
 import { nanoid } from "nanoid";
 
-const contactsPath = path.resolve("./db/contacts.json");
+const contactsPath = path.resolve("db", "contacts.json");
 
 const updateListContacts = (contacts) =>
   fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
