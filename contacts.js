254bf46f8063b39b3b22da90cf25d5623d255b65
Valerii <vd.alternative@gmail.com>
2024-03-09 19:23:54 +0200

    destruct

--- contacts.js
+++ contacts.js
@@ -22,7 +22,7 @@ export async function removeContact(id) {
   const contacts = await listContacts();
   const index = contacts.findIndex((contact) => contact.id === id);
   if (index === -1) return null;
-  const result = contacts.splice(index, 1);
+  const [result] = contacts.splice(index, 1);
 
   await updateListContacts(contacts);
   return result;
